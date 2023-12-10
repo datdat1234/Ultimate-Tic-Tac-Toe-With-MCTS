5e71191f4ad69a9ae43f32f572901cf20f264ac2
Deltora <deltora1st@gmail.com>
2023-12-10 12:39:00 +0700

    add move function and gitignore

--- MCTS.py
+++ MCTS.py
@@ -1,6 +1,8 @@
 import numpy as np
 from collections import defaultdict
 import copy
+from state import UltimateTTT_Move
+from state import State, State_2
 
 class MonteCarloTreeSearchNode():
     def __init__(self, state, parent=None, parent_action=None):
@@ -30,7 +32,7 @@ class MonteCarloTreeSearchNode():
     
     def expand(self):
         action = self._untried_actions.pop()
-        next_state = self.state.act_move_v2(action)
+        next_state = act_move_v2(self.state, action)
         child_node = MonteCarloTreeSearchNode(next_state, parent=self, parent_action=action)
         self.children.append(child_node)
         return child_node 
@@ -45,7 +47,7 @@ class MonteCarloTreeSearchNode():
             if len(possible_moves) == 0:
                 break
             action = self.rollout_policy(possible_moves)
-            current_rollout_state = current_rollout_state.act_move_v2(action)
+            current_rollout_state = act_move_v2(current_rollout_state, action)
         return current_rollout_state.game_result(current_rollout_state.global_cells.reshape(3,3))
     
     def backpropagate(self, result):
@@ -76,9 +78,35 @@ class MonteCarloTreeSearchNode():
         return current_node
     
     def best_action(self):
-        simulation_no = 100
+        simulation_no = 500
         for i in range(simulation_no):
             v = self._tree_policy()
             reward = v.rollout()
             v.backpropagate(reward)
         return self.best_child(c_param=1.414)
+
+'''
+    This is the move function where a new state 
+    will be created. You can implement this function
+    as a method of the State class. However, since I 
+    am unable to modify the state.py file because of 
+    the assignment requirements, I have placed the function here.
+'''
+
+def act_move_v2(state: State_2, move: UltimateTTT_Move):
+    state = copy.deepcopy(state)
+    if not state.is_valid_move(move):
+        raise ValueError(
+            "move {0} on local board is not valid".format(move)
+        )
+    local_board = state.blocks[move.index_local_board]
+    local_board[move.x, move.y] = move.value
+    
+    state.player_to_move *= -1          
+    state.previous_move = move
+    
+    if state.global_cells[move.index_local_board] == 0: # not 'X' or 'O'
+        if state.game_result(local_board):
+            state.global_cells[move.index_local_board] = move.value
+            
+    return state
